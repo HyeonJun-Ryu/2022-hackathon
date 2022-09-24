67af65f2b2e89d46af7defcf8d41489324f42f7a
Hyeonjun-Ryu <dignityr27601@gmail.com>
2022-09-24 17:25:18 +0900

    add Train API

---
+++ Train API/app.js
@@ -0,0 +1,10 @@
+// async function getData() {
+//     let rawResponse = await fetch("http://apis.data.go.kr/1613000/TrainInfoService/getStrtpntAlocFndTrainInfo?serviceKey=eVH%2BicBkGAKdXyNzKtOpFh6NioL1VfVJIjI0dqNc6OTpMhTeOknmcjjkW3HOFRUNK7qXYaGpIC7c42JRLEtsdQ%3D%3D&depPlaceId=NAT010000&arrPlaceId=NAT011668&depPlandTime=20220924&trainGradeCode=00&numOfRows=5&pageNo=1&_type=json");
+//     let jsonResponse = await rawResponse.json();
+//     //console.log(rawResponse);
+//     console.log(jsonResponse);
+//     document.write(jsonResponse.response);
+// }
+//
+// getData();
+
