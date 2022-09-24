8e465ac587c91a9fa878204417a0fe8e2e2a28a2
Hyeonjun-Ryu <dignityr27601@gmail.com>
2022-09-25 03:16:26 +0900

    fix

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports = { 
     googleProjectID: process.env.GOOGLE_PROJECT_ID,
     dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
     dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
