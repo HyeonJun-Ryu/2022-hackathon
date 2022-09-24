6b459c69febf783ffe4ebc4235d79778cbf35137
Hyeonjun-Ryu <dignityr27601@gmail.com>
2022-09-25 02:17:15 +0900

    fix

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server Running at ${port}`)
