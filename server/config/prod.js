e7357b34d26f9148270a9d8320e54bcad6cfb045
Hyeonjun-Ryu <dignityr27601@gmail.com>
2022-09-25 02:23:18 +0900

    fix

--- server/config/prod.js
+++ server/config/prod.js
@@ -2,7 +2,7 @@ module.exports = {
     googleProjectID: process.env.GOOGLE_PROJECT_ID,
     dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
     dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
-    googleClientEmail:process.env.googleClientEmail.GOOGLE_CLIENT_EMAIL,
+    googleClientEmail:process.env.googleClientEmail,
     googlePrivateKey: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
     mongoURI:process.env.MONGO_URI,
 }
